e295ec705fd717d34bab2f46ef5983c7deac894c
developer <shpandey@bestpeers.com>
2023-06-12 19:48:44 +0530

    Added job model and controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :jobs
+  root "jobs#index"
 end
