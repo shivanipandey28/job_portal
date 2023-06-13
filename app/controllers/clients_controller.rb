a6dc6095bf70d9fa33ebefc27822b4768843a104
developer <shpandey@bestpeers.com>
2023-06-13 13:58:40 +0530

    Added create action

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,4 +2,6 @@ class ClientsController < ApplicationController
   def index
     @clients = Client.all
   end
+  def show 
+    @client = Client.find(params[:id])
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,26 @@ class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
+
   def show
     @job = Job.find(params[:id])
+  end
+
+  def new 
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to @job
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def job_params
+    params.require(:job).permit(:job_title, :description)
   end
 end
